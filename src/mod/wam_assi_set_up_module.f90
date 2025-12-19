9ca974cd1cd08b5584289b20738fe175598ed402
RumYil <ruemeysa.yilmaz@hereon.de>
2025-12-19 16:50:56 +0100

    Update wam_assi_set_up_module.f90
    Spectral assimilation is set up

--- src/mod/wam_assi_set_up_module.f90
+++ src/mod/wam_assi_set_up_module.f90
@@ -40,19 +40,27 @@ USE WAM_OUTPUT_SET_UP_MODULE, ONLY: FFLAG20, FFLAG25, PFLAG20, PFLAG25
 
 IMPLICIT NONE
 
-INTEGER, PRIVATE :: LENT
+INTEGER, PRIVATE :: LENT, LENT_S
 
 ! ---------------------------------------------------------------------------- !
 !                                                                              !
 !     1. ASSIMILATION OPTIONS.                                                 !
 !        ---------------------                                                 !
-
-INTEGER             :: IASSI     !! ASSIMILATION FLAG
-                                 !!  = 1  FOR ASSIMILATION
-                                 !! OTHERWISE NO ASSIMILATION
+INTEGER             :: IASSI_SPECTRA       !! ASSIMILATION FLAG SPECTRA !! SA2025
+INTEGER             :: IASSI_ALTIMETER     !! ASSIMILATION FLAG         !! SA2025
+                                           !!  = 1  FOR ASSIMILATION
+                                           !! OTHERWISE NO ASSIMILATION
 REAL                :: DIST      !! RADIUS OF INFLUENCE IN DEGREES
 REAL                :: SIGOBS    !! MEASUREMENT SCATTER.
 REAL                :: SIGMOD    !! MODEL SCATTER.
+
+INTEGER             :: SARCORDIA !! CORRELATION DISTANCE IN METERS
+REAL                :: PPSARCF   !! CORRELATION DISTANCE COEFFICIENT
+REAL                :: COEFPHI   !! CROSS CORRELATION COEFFICIENT
+INTEGER             :: MPARTSW   !! MAXIMUM NUMBER O FPARTITIONINGS
+REAL                :: LOWEST  
+REAL                :: ALEVEL    
+REAL                :: FCUT
     
 INTEGER  :: LMAX   = 11          !! MAXIMUM NO. OF GRID UNITS TO SCAN FOR DATA
 
@@ -70,6 +78,16 @@ CHARACTER*14 :: CDATAE  = ' '    !! END DATE (VVYYMMDDHHMMSS).
 CHARACTER*14 :: CDTASS  = ' '    !! DATE OF NEXT ASSIMILATION.
 INTEGER      :: IDELASS = -1     !! TIMESTEP ASSIMILATION IN SECONDS.
 
+!     2.1 SPECTRAL ASSIMILATION TIME STATUS                             !! SA2025!
+!         ---------------------------------
+
+CHARACTER*14 :: CDATSA  = ' '    !! START DATE  (VVYYMMDDHHMMSS).
+CHARACTER*14 :: CDATSE  = ' '    !! END DATE (VVYYMMDDHHMMSS).
+CHARACTER*14 :: CDTSPS  = ' '    !! DATE OF NEXT ASSIMILATION.
+INTEGER      :: IDELSPE = -1     !! TIMESTEP ASSIMILATION IN SECONDS.
+
+
+
 ! ---------------------------------------------------------------------------- !
 !                                                                              !
 !     3. OBSERVATION UNIT AND FILE IDENTIFIER.                                 !
@@ -79,9 +97,14 @@ INTEGER      :: IDELASS = -1     !! TIMESTEP ASSIMILATION IN SECONDS.
 !  THE FULL FILE NAME IS THE FILE IDENTIFIER EXTENDED BY THE FILE DATE.        !
 !  SEE GFILE FOR DETAILS.                                                      !
 
+!  ALTIMETER DATA
 INTEGER      :: IU80   = 80           !! OBSERVATION FILE.
 CHARACTER*80 :: FILE80 = 'OBS'        !! OBSERVATION FILE IDENTIFIER.
-			
+
+! SPECTRAL DATA
+INTEGER      :: IU90   = 90           !! SPECTRAL OBSERVATION FILE.
+CHARACTER(LEN=80) :: FILE90 = 'SWI'   !! SPECTRAL OBSERVATION FILE IDENTIFIER.   			
+
 LOGICAL      :: FG_OUTPUT = .false.   !! FIRST GUESS OUTPUT FLAG.
 
 INTEGER      :: IU30 = 30             !! FIRST GUESS OUTPUT UNIT FOR 
@@ -98,10 +121,20 @@ CHARACTER*80 :: FILE35 = 'OUTFG'      !! FILE IDENTIFIER
 !                                                                              !
 ! ---------------------------------------------------------------------------- !
 
-INTERFACE SET_ASSIMILATION_OPTION             !! SETS ASSIMILATION OPTIONS.
-   MODULE PROCEDURE SET_ASSIMILATION_OPTION
+INTERFACE SET_ASSIMILATION_OPTION_ALTIMETER   !! SETS ASSIMILATION OPTIONS FOR ALTIMETER.
+   MODULE PROCEDURE SET_ASSIMILATION_OPTION_ALTIMETER   !! SA2025
 END INTERFACE
-PUBLIC SET_ASSIMILATION_OPTION
+PUBLIC SET_ASSIMILATION_OPTION_ALTIMETER
+
+INTERFACE SET_ASSIMILATION_OPTION_SPECTRA   !! SETS ASSIMILATION OPTION FOR SPECTRA
+   MODULE PROCEDURE SET_ASSIMILATION_OPTION_SPECTRA
+END INTERFACE
+PUBLIC SET_ASSIMILATION_OPTION_SPECTRA
+
+INTERFACE SET_ASSIMILATION_PERIOD_SPECTRA              !! SETS SPECTRAL ASSIMILATION PERIOD.
+   MODULE PROCEDURE SET_ASSIMILATION_PERIOD_SPECTRA
+END INTERFACE
+PUBLIC SET_ASSIMILATION_PERIOD_SPECTRA
 
 INTERFACE SET_ASSIMILATION_OUTPUT           !! SETS ASSIMILATION FG_OUTPUT.
    MODULE PROCEDURE SET_ASSIMILATION_OUTPUT
@@ -113,10 +146,16 @@ INTERFACE SET_ASSIMILATION_PERIOD              !! SETS ASSIMILATION PERIOD.
 END INTERFACE
 PUBLIC SET_ASSIMILATION_PERIOD
 
-INTERFACE SET_OBSERVATION_FILE           !! SETS OBSERVATION FILE IDENTIFIER.
-   MODULE PROCEDURE SET_OBSERVATION_FILE
+INTERFACE SET_ALTIMETER_OBSERVATION_FILE           !! SETS OBSERVATION FILE IDENTIFIER.
+   MODULE PROCEDURE SET_ALTIMETER_OBSERVATION_FILE
 END INTERFACE
-PUBLIC SET_OBSERVATION_FILE
+PUBLIC SET_ALTIMETER_OBSERVATION_FILE
+
+INTERFACE SET_SPECTRAL_OBSERVATION_FILE           !! SETS OBSERVATION FILE IDENTIFIER.
+   MODULE PROCEDURE SET_SPECTRAL_OBSERVATION_FILE
+END INTERFACE
+PUBLIC SET_SPECTRAL_OBSERVATION_FILE
+
 
 INTERFACE SET_ASSI_MAP_FILE
    MODULE PROCEDURE SET_ASSI_MAP_FILE
@@ -138,6 +177,11 @@ INTERFACE PREPARE_ASSIMILATION             !! PREPARE ASSIMILATION.
 END INTERFACE
 PUBLIC PREPARE_ASSIMILATION
 
+INTERFACE PREPARE_SPECTRAL_ASSIMILATION             !! PREPARE ASSIMILATION.
+   MODULE PROCEDURE PREPARE_SPECTRAL_ASSIMILATION
+END INTERFACE
+PUBLIC PREPARE_SPECTRAL_ASSIMILATION
+
 ! ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ !
 !                                                                              !
 !     E.  PRIVATE INTERFACES.                                                  !
@@ -154,7 +198,81 @@ CONTAINS
 !                                                                              !
 ! ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ !
 
-SUBROUTINE SET_ASSIMILATION_OPTION (AS, RA, SO, SM)
+SUBROUTINE SET_ASSIMILATION_OPTION_SPECTRA (AS_SPEC, CD, SC, CC, MP, LT, AL, F_CUT) !! SA2025
+
+INTEGER, INTENT(IN)           :: AS_SPEC  !! ASSIMILATION FLAG
+                                          !!  = 1  FOR ASSIMILATION
+                                          !! OTHERWISE NO ASSIMILATION
+INTEGER,    INTENT(IN), OPTIONAL :: CD    !! CORRELATION DISTANCE IN METERS
+REAL,       INTENT(IN), OPTIONAL :: SC    !! CORRELATION DISTANCE COEFFICIENT
+REAL,       INTENT(IN), OPTIONAL :: CC    !! CROSS CORRELATION COEFFICIENT 
+INTEGER,    INTENT(IN), OPTIONAL :: MP    !! MAXIMUM NUMBER OF PARTITIONINGS 
+REAL,       INTENT(IN), OPTIONAL :: LT    !! FACTOR FOR THRESHOLD CHECK
+REAL,       INTENT(IN), OPTIONAL :: AL    !! MINIMUM DISTANCE BETWEEN TWO WAVE
+!                                            SYSTEMS IN K-SPACE FOR COMBINING
+!                                            MODEL AND OBSERVATIONS PEAKS.
+REAL,       INTENT(IN), OPTIONAL :: F_CUT  !! CUTOFF FREQUENCY      
+                              
+IF (AS_SPEC.EQ.1) THEN
+   IASSI_SPECTRA = 1   !! SA2025
+ELSE
+   IASSI_SPECTRA = 0   !! SA2025
+END IF
+
+IF (AS_SPEC.NE.1) RETURN
+
+IF (.NOT.PRESENT(CD) .OR. .NOT. PRESENT(SC) .OR. .NOT. PRESENT(CC) .OR. .NOT. PRESENT(MP) & 
+&   .OR. .NOT. PRESENT(LT) .OR. .NOT. PRESENT(AL) .OR. .NOT. PRESENT(F_CUT))     THEN
+   WRITE(IU06,*) ' *******************************************************'
+   WRITE(IU06,*) ' *                                                     *'
+   WRITE(IU06,*) ' *FATAL ERROR IN SUB. SET_ASSIMILATION_OPTION_SPECTRA  *'
+   WRITE(IU06,*) ' *=====================================================*'
+   WRITE(IU06,*) ' * A SPECTRAL ASSIMILATION RUN IS REQUESTED BUT        *'
+   WRITE(IU06,*) ' * CORRELATION DISTANCE(SARCORDIA)                     *'
+   WRITE(IU06,*) ' * AND / OR                                            *'
+   WRITE(IU06,*) ' * CORRELATION DISTANCE COEFFICIENT(PPSARCF)           *'
+   WRITE(IU06,*) ' * AND / OR                                            *'
+   WRITE(IU06,*) ' * CROSS CORRELATION COEFFICIENT(COEFPHI)              *' 
+   WRITE(IU06,*) ' * AND / OR                                            *'
+   WRITE(IU06,*) ' * MAXIMUM NUMBER OF PARTITIONINGS(MPARTSW)            *'
+   WRITE(IU06,*) ' * AND / OR                                            *'
+   WRITE(IU06,*) ' * FACTOR FOR THRESHOLD CHECK(LOWEST)                  *'
+   WRITE(IU06,*) ' * AND / OR                                            *'
+   WRITE(IU06,*) ' * MINIMUM DISTANCE FOR PEAK COMBINING(ALEVEL)         *'   
+   WRITE(IU06,*) ' * AND / OR                                            *'    
+   WRITE(IU06,*) ' * CUTOFF FREQUENCY(FCUT)                              *'    
+   WRITE(IU06,*) ' * IS NOT DEFINED IN WAM_USER FILE                     *'
+   WRITE(IU06,*) ' *                                                     *'
+   WRITE(IU06,*) ' *         PROGRAM ABORTS  PROGRAM ABORTS              *'
+   WRITE(IU06,*) ' *                                                     *'
+   WRITE(IU06,*) ' *******************************************************'
+   CALL ABORT1
+END IF
+
+SARCORDIA = CD
+PPSARCF   = SC
+COEFPHI   = CC
+MPARTSW   = MP
+LOWEST    = LT
+ALEVEL    = AL
+FCUT      = F_CUT
+END SUBROUTINE SET_ASSIMILATION_OPTION_SPECTRA
+
+
+SUBROUTINE SET_ASSIMILATION_PERIOD_SPECTRA (C, D, STEP_SPEC)
+
+CHARACTER*14, INTENT(IN)   :: C     !! START DATE OF ASSIMILATION.
+CHARACTER*14, INTENT(IN)   :: D     !! END DATE OF ASSIMILATION.
+INTEGER,      INTENT(IN)   :: STEP_SPEC  !!MILATION TIMSTEP [S].
+
+CDATSA  = C
+CDATSE  = D
+IDELSPE = STEP_SPEC
+
+END SUBROUTINE SET_ASSIMILATION_PERIOD_SPECTRA
+
+
+SUBROUTINE SET_ASSIMILATION_OPTION_ALTIMETER (AS, RA, SO, SM) !! SA2025
 
 INTEGER, INTENT(IN)           :: AS  !! ASSIMILATION FLAG
                                      !!  = 1  FOR ASSIMILATION
@@ -164,9 +282,9 @@ REAL,    INTENT(IN), OPTIONAL :: SO  !! OBSERVATION SCATTER
 REAL,    INTENT(IN), OPTIONAL :: SM  !! MODEL SCATTER
                                     
 IF (AS.EQ.1) THEN
-   IASSI = 1
+   IASSI_ALTIMETER = 1   !! SA2025
 ELSE
-   IASSI = 0
+   IASSI_ALTIMETER = 0   !! SA2025
 END IF
 
 IF (AS.NE.1) RETURN
@@ -174,8 +292,8 @@ IF (AS.NE.1) RETURN
 IF (.NOT.PRESENT(RA) .OR. .NOT. PRESENT(SO) .OR. .NOT. PRESENT(SM)) THEN
    WRITE(IU06,*) ' *******************************************************'
    WRITE(IU06,*) ' *                                                     *'
-   WRITE(IU06,*) ' *     FATAL ERROR IN SUB. SET_ASSIMILATION_OPTION     *'
-   WRITE(IU06,*) ' *     ===========================================     *'
+   WRITE(IU06,*) ' *FATAL ERROR IN SUB. SET_ASSIMILATION_OPTION_ALTIMETER*'
+   WRITE(IU06,*) ' *=====================================================*'
    WRITE(IU06,*) ' * AN ASSIMILATION RUN IS REQUESTED BUT                *'
    WRITE(IU06,*) ' * RADIUS OF INFLUENCE                                 *'
    WRITE(IU06,*) ' * AND / OR                                            *'
@@ -194,7 +312,7 @@ DIST   = RA*RAD
 SIGOBS = SO
 SIGMOD = SM
 
-END SUBROUTINE SET_ASSIMILATION_OPTION
+END SUBROUTINE SET_ASSIMILATION_OPTION_ALTIMETER
 
 ! ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ !
 
@@ -222,7 +340,7 @@ END SUBROUTINE SET_ASSIMILATION_PERIOD
 
 ! ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ !
 
-SUBROUTINE SET_OBSERVATION_FILE (NAME, UNIT)
+SUBROUTINE SET_ALTIMETER_OBSERVATION_FILE (NAME, UNIT)
 
 ! ---------------------------------------------------------------------------- !
 !                                                                              !
@@ -238,7 +356,28 @@ LENT = LEN_TRIM(NAME)
 IF (LENT.GT.0) FILE80 = NAME(1:LENT)
 IF (PRESENT(UNIT)) IU80 = UNIT
 
-END SUBROUTINE SET_OBSERVATION_FILE
+END SUBROUTINE SET_ALTIMETER_OBSERVATION_FILE
+
+! ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ !
+
+SUBROUTINE SET_SPECTRAL_OBSERVATION_FILE (NAME_S, UNIT_S)
+
+! ---------------------------------------------------------------------------- !
+!                                                                              !
+!     INTERFACE VARIABLES.                                                     !
+!     --------------------                                                     !
+
+CHARACTER, INTENT(IN)           :: NAME_S*(*)  !! FILE NAME
+INTEGER,   INTENT(IN), OPTIONAL :: UNIT_S      !! UNIT NUMBER
+
+! ---------------------------------------------------------------------------- !
+
+LENT_S = LEN_TRIM(NAME_S)
+IF (LENT_S.GT.0) FILE90 = NAME_S(1:LENT_S)
+IF (PRESENT(UNIT_S)) IU90 = UNIT_S
+
+END SUBROUTINE SET_SPECTRAL_OBSERVATION_FILE
+
 
 ! ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ !
 
@@ -292,12 +431,12 @@ WRITE(IU06,*) '              MODEL ASSIMILATION STATUS:'
 WRITE(IU06,*) ' ------------------------------------------------- '
 
 WRITE(IU06,*) '  '
-IF (IASSI.NE.1) THEN
-   WRITE(IU06,*) ' MODEL RUNS WITHOUT ASSIMILATION'
+IF (IASSI_ALTIMETER.NE.1) THEN   !! SA2025
+   WRITE(IU06,*) ' MODEL RUNS WITHOUT ALTIMETER ASSIMILATION'
    WRITE(IU06,*) '  '
    RETURN
 ELSE
-   WRITE(IU06,*) ' MODEL RUNS WITH ASSIMILATION'
+   WRITE(IU06,*) ' MODEL RUNS WITH ALTIMETER ASSIMILATION'
 END IF
 WRITE(IU06,*) ' RADIUS OF INFLUENCE................: ', DIST*DEG,' DEG'
 WRITE(IU06,*) ' OBSERVATION SCATTER................: ', SIGOBS
@@ -308,8 +447,32 @@ WRITE(IU06,*) ' END   DATE (YYYYMMDDHHMMSS) IS.....: ', CDATAE
 WRITE(IU06,*) ' DATE OF NEXT ASSIMILATION   IS.....: ', CDTASS
 WRITE(IU06,*) ' ASSIMILATION TIME STEP ............: ', IDELASS,' SECS'
 
+!---------------------Spectral Assimilation---------------------!
+
+WRITE(IU06,*) '  '
+IF (IASSI_SPECTRA.NE.1) THEN       !! SA2025
+   WRITE(IU06,*) ' MODEL RUNS WITHOUT SPECTRAL ASSIMILATION'
+   WRITE(IU06,*) '  '
+   RETURN
+ELSE
+   WRITE(IU06,*) ' MODEL RUNS WITH SPECTRAL ASSIMILATION'
+END IF
+WRITE(IU06,*) ' MAX CORRELATION DISTANCE...........: ', SARCORDIA,' M'
+WRITE(IU06,*) ' MAX INFLUENCE DISTANCE.............: ', PPSARCF*SARCORDIA, ' M'
+WRITE(IU06,*) ' PHI COEFFICIENT....................: ', COEFPHI
+WRITE(IU06,*) ' MAX NUMBER OF PARTITIONINGS........: ', MPARTSW
+WRITE(IU06,*) ' THRESHHOLD FACTOR..................: ', LOWEST
+WRITE(IU06,*) ' MIN DIST. BETWEEN WAVE SYSTEMS (k).: ', ALEVEL
+WRITE(IU06,*) ' CUTOFF FREQUENCY...................: ', FCUT
+WRITE(IU06,*) '  '
+WRITE(IU06,*) ' START DATE (YYYYMMDDHHMMSS) IS.....: ', CDATSA
+WRITE(IU06,*) ' END   DATE (YYYYMMDDHHMMSS) IS.....: ', CDATSE
+WRITE(IU06,*) ' DATE OF NEXT ASSIMILATION   IS.....: ', CDTSPS
+WRITE(IU06,*) ' ASSIMILATION TIME STEP ............: ', IDELSPE,' SECS'
+
+
 LENT = LEN_TRIM(FILE80)
-WRITE (IU06,'(''  OBSERVATION FILE .. UNIT:'',I3,'', ID .: '',A)')             &
+WRITE (IU06,'('' ALTIMETER OBSERVATION FILE .. UNIT:'',I3,'', ID .: '',A)')             &
 &                                                         IU80, FILE80(1:LENT)
 INQUIRE (UNIT=IU80, OPENED=OPND)
 IF (OPND) THEN
@@ -318,6 +481,18 @@ ELSE
    WRITE(IU06,*) '  FILE IS NOT ASSIGNED'
 END IF
 WRITE(IU06,*) '  '
+
+LENT_S = LEN_TRIM(FILE90)
+WRITE (IU06,'('' SPECTRA OBSERVATION FILE .. UNIT:'',I3,'', ID .: '',A)')    &
+&                                                         IU90, FILE90(1:LENT_S)
+INQUIRE (UNIT=IU90, OPENED=OPND)
+IF (OPND) THEN
+   WRITE(IU06,*) ' SPECTRA  FILE IS ASSIGNED'
+ELSE
+   WRITE(IU06,*) ' SPECTRA FILE IS NOT ASSIGNED'
+END IF
+WRITE(IU06,*) '  '
+
 
 IF ((FFLAG20 .OR. PFLAG20) .AND. FG_OUTPUT) THEN
    WRITE(IU06,*) '  OUTPUT OF FIRST GUESS INTEGRATED PARAMETERS'
@@ -339,6 +514,37 @@ END SUBROUTINE PRINT_ASSIMILATION_STATUS
 
 ! ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ !
 
+SUBROUTINE PREPARE_SPECTRAL_ASSIMILATION
+! ---------------------------------------------------------------------------- !
+! ---------------------------------------------------------------------------- !
+! ------------------------- SPECTRAL ASSIMILATION ---------------------------- !
+!! SA2025
+ 
+CDTSPS = CDATSA
+DO WHILE (CDTSPS.LT.CDTPRO)
+   CALL INCDATE(CDTSPS,IDELSPE)
+END DO
+
+
+IF (CDTSPS.GT.CDATSE) THEN
+   WRITE(IU06,*) ' *******************************************************'
+   WRITE(IU06,*) ' *                                                     *'
+   WRITE(IU06,*) ' *     FATAL ERROR IN SUB. PREPARE_ASSIMILATION        *'
+   WRITE(IU06,*) ' *     ========================================        *'
+   WRITE(IU06,*) ' *                                                     *'
+   WRITE(IU06,*) ' * DATES DO NOT MATCH IN SPECTRAL ASSIMILATION         *'
+   WRITE(IU06,*) ' * START DATE OF SPECTRAL ASSIMILATION IS: ', CDATSA
+   WRITE(IU06,*) ' * END   DATE OF SPECTRAL ASSIMILATION IS: ', CDATSE
+   WRITE(IU06,*) ' * MODEL DATE OF                       IS: ', CDTPRO
+   WRITE(IU06,*) ' *                                                     *'
+   WRITE(IU06,*) ' *         PROGRAM ABORTS  PROGRAM ABORTS              *'
+   WRITE(IU06,*) ' *                                                     *'
+   WRITE(IU06,*) ' *******************************************************'
+   CALL ABORT1
+ENDIF
+!! SA2025 
+END SUBROUTINE PREPARE_SPECTRAL_ASSIMILATION
+
 SUBROUTINE PREPARE_ASSIMILATION
 
 ! ---------------------------------------------------------------------------- !
@@ -348,8 +554,6 @@ SUBROUTINE PREPARE_ASSIMILATION
 
 INTEGER :: J
 REAL    :: DELLON
-
-! ---------------------------------------------------------------------------- !
 
 CDTASS = CDATAA
 DO WHILE (CDTASS.LT.CDTPRO)
