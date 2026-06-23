0c6c01f05474b7b3cec4399ed3354e5dd71c166b
dlaparna7 <dlaparna07@gmail.com>
2026-06-23 16:05:49 +0200

    fix: Fixed array indexing to match the indexing style in the rest of the code

--- src/mod/wam_wind_implementation_submodule.f90
+++ src/mod/wam_wind_implementation_submodule.f90
@@ -1,6 +1,7 @@
 submodule (wam_wind_module) wam_wind_implementation_submodule
 
   use wam_netcdf_input_reader, only : read_wind_fields
+  USE WAM_MPI_MODULE,     ONLY: NIJS, NIJL
   implicit none
 
 contains
@@ -49,8 +50,8 @@ end if
 
 IF (EQUAL_GRID) THEN
    DO IJ = NIJS, NIJL
-      US(IJ)= U_IN(IFROMIJ(IJ),KFROMIJ(IJ))
-      DS(IJ)= V_IN(IFROMIJ(IJ),KFROMIJ(IJ))
+      US(IJ-NIJS+1)= U_IN(IFROMIJ(IJ),KFROMIJ(IJ))
+      DS(IJ-NIJS+1)= V_IN(IFROMIJ(IJ),KFROMIJ(IJ))
    END DO
 ELSE
    CALL INTERPOLATION_TO_GRID (US, DS)
@@ -61,15 +62,15 @@ END IF
 !         -------------------------------------                                !
 
 DO IJ = NIJS, NIJL
-   UU = US(IJ)
-   VV = DS(IJ)
-   US(IJ) = SQRT(UU**2 + VV**2)
-   IF (US(IJ).NE.0.) THEN
-      DS(IJ) = ATAN2(UU,VV)
+   UU = US(IJ-NIJS+1)
+   VV = DS(IJ-NIJS+1)
+   US(IJ-NIJS+1) = SQRT(UU**2 + VV**2)
+   IF (US(IJ-NIJS+1).NE.0.) THEN
+      DS(IJ-NIJS+1) = ATAN2(UU,VV)
    ELSE
-      DS(IJ) = 0.
+      DS(IJ-NIJS+1) = 0.
    ENDIF
-   IF (DS(IJ).LT.0.) DS(IJ) = DS(IJ) + ZPI
+   IF (DS(IJ-NIJS+1).LT.0.) DS(IJ-NIJS+1) = DS(IJ-NIJS+1) + ZPI
 END DO
 
 ! ---------------------------------------------------------------------------- !
@@ -84,10 +85,10 @@ IF (CODE_IN.EQ.1) THEN
 !          ---------------------------                                         !
 
    DO IJ = NIJS, NIJL
-         USTAR = MAX(0.01,US(IJ))
+         USTAR = MAX(0.01,US(IJ-NIJS+1))
          Z0  = ALPHACH/G*USTAR**2
          CD  = XKAPPA/ALOG(10./Z0)
-         US(IJ) = USTAR/CD
+         US(IJ-NIJS+1) = USTAR/CD
    END DO
 
 ELSE IF (CODE_IN.EQ.2) THEN
@@ -96,14 +97,14 @@ ELSE IF (CODE_IN.EQ.2) THEN
 !         ---------------------------------                                    !
 !                                                                              !
    DO IJ = NIJS, NIJL
-         USTAR = MAX (0.01, SQRT(US(IJ)/ROAIR))
+         USTAR = MAX (0.01, SQRT(US(IJ-NIJS+1)/ROAIR))
          Z0  = ALPHACH/G*USTAR**2
          CD  = XKAPPA/ALOG(10./Z0)
-         US(IJ) = USTAR/CD
+         US(IJ-NIJS+1) = USTAR/CD
    END DO
 END IF
 
-US(NIJS:NIJL)  = MAX(US(NIJS:NIJL), 2.0)
+US(:)  = MAX(US(:), 2.0)
 
 ! ---------------------------------------------------------------------------- !
 !                                                                              !
@@ -115,8 +116,8 @@ IF (ITEST.GE.3) THEN
    WRITE (IU06,*) ' '
    WRITE (IU06,*) '      SUB. WAM_WIND: WINDFIELDS CONVERTED TO MODEL GRID'
    WRITE (IU06,*) ' '
-   WRITE (IU06,*) ' US(NIJS:NIJS+10) = ', US(NIJS:IJ)
-   WRITE (IU06,*) ' DS(NIJS:NIJS+10) = ', DS(NIJS:IJ)
+   WRITE (IU06,*) ' US(NIJS:NIJS+10) = ', US(1:IJ-NIJS+1)
+   WRITE (IU06,*) ' DS(NIJS:NIJS+10) = ', DS(1:IJ-NIJS+1)
 END IF
 
 end procedure wam_wind
